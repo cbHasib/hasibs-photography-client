0d26208c69935b035a840763203b0a5f25c7957f
cbHasib <hasibul.pbn@gmail.com>
2022-11-09 12:58:18 +0600

    Update Service Card

--- src/Pages/Services/ServiceCard/ServiceCard.js
+++ src/Pages/Services/ServiceCard/ServiceCard.js
@@ -1,9 +1,15 @@
-import { Button } from "flowbite-react";
+import { Button, Rating } from "flowbite-react";
 import React from "react";
 
 const ServiceCard = () => {
+  const serviceDetails =
+    "Full preparation guide on HSC Physics 1st Paper Syllabus. Ensure maximum preparation in the shortest possible time. Enroll now and secure an A+ in HSC Physics 1st Paper.";
   return (
-    <div className="max-w-[380px] w-full bg-white rounded-md shadow-md dark:bg-slate-800 dark:text-gray-100 p-3 flex flex-col justify-between text-center hover:-translate-y-1 duration-300">
+    <div className="max-w-[380px] w-full bg-white rounded-md shadow-md dark:bg-slate-800 dark:text-gray-100 p-3 flex flex-col justify-between text-center hover:-translate-y-1 duration-300 relative">
+      <div className="bg-orange-500 text-white p-1.5 rounded-full absolute top-1 right-1">
+        <span className="font-serif">৳</span>
+        {"764"}
+      </div>
       <div>
         <img
           src={"https://flowbite.com/docs/images/blog/image-1.jpg"}
@@ -14,7 +20,17 @@ const ServiceCard = () => {
           <h2 className="text-xl font-semibold tracking-wide text-ellipsis ">
             {"Wedding Photography"}
           </h2>
+          <div className="flex justify-center items-center">
+            <Rating>
+              <Rating.Star />
+              <Rating.Star />
+              <Rating.Star />
+              <Rating.Star />
+              <Rating.Star filled={false} />
+            </Rating>
+          </div>
         </div>
+        <p className="">{serviceDetails.slice(0, 100) + "..."}</p>
       </div>
 
       <div className="px-3 py-2 space-y-3">
